f1501eed58fbded35aa47ad63d6009e90ac27343
Yash Rakeshbhai Patel <24dcs096@charusat.edu.in>
2025-09-27 01:42:06 +0530

    login.php
    login.php

--- login.php
+++ login.php
@@ -1,9 +1,24 @@
 <?php
 session_start();
+
 if (!isset($_POST['name']) || strlen(trim($_POST['name'])) < 1) {
     header("Location: index.php?error=Name+is+required");
     exit();
 }
+
+// Save the user name in session
 $_SESSION['name'] = trim($_POST['name']);
+
+// Redirect to game page with name
 header("Location: game.php");
 exit();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Rock Paper Scissors - ba63c271</title>
+</head>
+<body>
+<!-- This page should never be visible since it redirects -->
+</body>
+</html>
